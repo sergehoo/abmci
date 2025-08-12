dc0ea37a2e40debc33faa50bbe02e1ad89af57b8
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-08-12 09:12:02 +0000

    ase

--- abmci/settings/base.py
+++ abmci/settings/base.py
@@ -2,14 +2,14 @@
 import os
 from pathlib import Path
 from datetime import timedelta
-from decouple import config, Csv
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-SECRET_KEY = config("SECRET_KEY")
-DEBUG = config("DEBUG", cast=bool, default=False)
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv(), default=["localhost", "127.0.0.1"])
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -81,12 +81,12 @@ ASGI_APPLICATION = "abmci.asgi.application"
 # DB: par défaut sqlite (override en prod)
 DATABASES = {
     "default": {
-        "ENGINE": config("DB_ENGINE", default="django.db.backends.sqlite3"),
-        "NAME": config("DB_NAME", default=str(BASE_DIR / "db.sqlite3")),
-        "USER": config("DB_USER", default=""),
-        "PASSWORD": config("DB_PASSWORD", default=""),
-        "HOST": config("DB_HOST", default=""),
-        "PORT": config("DB_PORT", default=""),
+        "ENGINE": os.environ.get("DB_ENGINE", default="django.db.backends.sqlite3"),
+        "NAME": os.environ.get("DB_NAME", default=str(BASE_DIR / "db.sqlite3")),
+        "USER": os.environ.get("DB_USER", default=""),
+        "PASSWORD": os.environ.get("DB_PASSWORD", default=""),
+        "HOST": os.environ.get("DB_HOST", default=""),
+        "PORT": os.environ.get("DB_PORT", default=""),
     }
 }
 
@@ -117,15 +117,15 @@ REST_AUTH = {
     "USER_DETAILS_SERIALIZER": "api.serializers.CustomUserDetailsSerializer",
 }
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=config("JWT_ACCESS_MIN", cast=int, default=60)),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=config("JWT_REFRESH_DAYS", cast=int, default=7)),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=os.environ.get("JWT_ACCESS_MIN")),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=os.environ.get("JWT_REFRESH_DAYS")),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
-SITE_ID = config("SITE_ID", cast=int, default=1)
+SITE_ID = os.environ.get("SITE_ID")
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", cast=Csv(), default=[])
-CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", cast=Csv(), default=[])
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS")
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS")
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
@@ -137,15 +137,15 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = config("EMAIL_HOST", default="")
-EMAIL_PORT = config("EMAIL_PORT", cast=int, default=465)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=False)
-EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool, default=True)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="")
+EMAIL_HOST = os.environ.get("EMAIL_HOST", default="")
+EMAIL_PORT = os.environ.get("EMAIL_PORT", cast=int, default=465)
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", cast=bool, default=False)
+EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", cast=bool, default=True)
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", default="")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", default="")
 
-SESSION_COOKIE_AGE = config("SESSION_COOKIE_AGE", cast=int, default=60*60*24*30)
+SESSION_COOKIE_AGE = os.environ.get("SESSION_COOKIE_AGE", cast=int, default=60*60*24*30)
 
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
@@ -166,21 +166,21 @@ CHANNEL_LAYERS = {
 }
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-CSRF_COOKIE_SECURE = config("CSRF_COOKIE_SECURE", cast=bool, default=False)
-SESSION_COOKIE_SECURE = config("SESSION_COOKIE_SECURE", cast=bool, default=False)
+CSRF_COOKIE_SECURE = os.environ.get("CSRF_COOKIE_SECURE", cast=bool, default=False)
+SESSION_COOKIE_SECURE = os.environ.get("SESSION_COOKIE_SECURE", cast=bool, default=False)
 
 # ====== ORANGE SMS ======
-ORANGE_TOKEN_URL = config("ORANGE_TOKEN_URL", default="https://api.orange.com/oauth/v3/token")
-ORANGE_SMS_URL = config("ORANGE_SMS_URL", default="https://api.orange.com/smsmessaging/v1/outbound/{}/requests")
-ORANGE_SMS_CLIENT_ID = config("ORANGE_SMS_CLIENT_ID", default="")
-ORANGE_SMS_CLIENT_SECRET = config("ORANGE_SMS_CLIENT_SECRET", default="")
-ORANGE_SMS_SENDER = config("ORANGE_SMS_SENDER", default="")  # ex: tel:+225734201
+ORANGE_TOKEN_URL = os.environ.get("ORANGE_TOKEN_URL", default="https://api.orange.com/oauth/v3/token")
+ORANGE_SMS_URL = os.environ.get("ORANGE_SMS_URL", default="https://api.orange.com/smsmessaging/v1/outbound/{}/requests")
+ORANGE_SMS_CLIENT_ID = os.environ.get("ORANGE_SMS_CLIENT_ID", default="")
+ORANGE_SMS_CLIENT_SECRET = os.environ.get("ORANGE_SMS_CLIENT_SECRET", default="")
+ORANGE_SMS_SENDER = os.environ.get("ORANGE_SMS_SENDER", default="")  # ex: tel:+225734201
 
 # ====== META WhatsApp Cloud API ======
-META_WA_API_VERSION = config("META_WA_API_VERSION", default="v20.0")
-META_WA_BASE_URL = config("META_WA_BASE_URL", default="https://graph.facebook.com")
-META_WA_PHONE_NUMBER_ID = config("META_WA_PHONE_NUMBER_ID", default="")
-META_WA_ACCESS_TOKEN = config("META_WA_ACCESS_TOKEN", default="")
+META_WA_API_VERSION = os.environ.get("META_WA_API_VERSION", default="v20.0")
+META_WA_BASE_URL = os.environ.get("META_WA_BASE_URL", default="https://graph.facebook.com")
+META_WA_PHONE_NUMBER_ID = os.environ.get("META_WA_PHONE_NUMBER_ID", default="")
+META_WA_ACCESS_TOKEN = os.environ.get("META_WA_ACCESS_TOKEN", default="")
 
 # Logging minimal (override prod)
 LOGGING = {
