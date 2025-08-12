912a2b7105b47819249742dc81679085164cec69
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-08-12 19:13:49 +0000

    adr

--- abmci/celery.py
+++ abmci/celery.py
@@ -1,4 +1,6 @@
 import os
+from celery import Celery
+
 # from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "abmci.settings")
--- abmci/tasks.py
+++ abmci/tasks.py
@@ -1,4 +1,4 @@
-from celery import shared_task
+from abmci.celery import shared_task
 from django.utils import timezone
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
