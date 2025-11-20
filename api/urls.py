a9038abf7c1adabb1e7a4affd86e029325d5d0d9
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-11-20 00:09:15 +0000

    ae

--- api/urls.py
+++ api/urls.py
@@ -20,7 +20,7 @@ router.register(r'devices', DeviceViewSet, basename='devices')
 router.register("versions", BibleVersionViewSet, basename="bible-version")
 router.register("verses", BibleVerseViewSet, basename="bible-verse")
 router.register(r'bible/tags', BibleTagViewSet, basename='bible-tag')
-router.register(r"notifications", UserNotificationViewSet, basename="notifications")
+router.register(r"notifications", UserNotificationViewSet, basename="user-notifications")
 router.register(r"problems/categories", ProblemCategoryViewSet, basename="problem-category")
 router.register(r"problems/reports", ProblemReportViewSet, basename="problem-report")
 
